db465f21ad8ff4dadc076f8c23879c4a2c4339fd
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2025-02-05 16:46:54 -0500

    Homework Class 3

---
+++ homeworks/CLASS 3/homework_vainilla_web_java/src/main/java/com/eafit/web/AboutServlet.java
@@ -0,0 +1,29 @@
+package com.eafit.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException; 
+import java.io.PrintWriter;
+
+// @WebServlet("/about")
+public class AboutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Home</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Welcome to the About Page</h1>");
+        out.println("<p>This is a simple web application using Servlets and it's located at /about</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
